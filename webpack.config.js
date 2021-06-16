1ef558d351fcec254e805d0b63d1c288f6f7ff7a
Mario Freire <mjfneto@gmail.com>
2021-06-16 18:39:20 -0300

    Load CSS

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,18 @@
 const path = require("path");
 
 module.exports = {
+  mode: "production",
   entry: "./src/index.js",
   output: {
-    filename: "main.js",
+    filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
-  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
 };
